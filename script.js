a6adbaea0faeab69a7ee0a6d8f3beebdef642e90
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-05-04 15:56:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var element = document.getElementById("jonas");
+console.log(element);
+
+var element2 = document.getElementById("jocas");
+console.log(element2);
+
+var element3 = document.getElementById("macaco");
+console.log(element3);
